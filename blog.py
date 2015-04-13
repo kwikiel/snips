51939e67898105c00349e36223be3927abb10f57
Kacper Wikieł <random8902@gmail.com>
2015-04-13 22:32:12 +0200

    Navbar with navigation

--- blog.py
+++ blog.py
@@ -8,7 +8,7 @@ db = SQLAlchemy(app)
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(140), unique=True)
+    title = db.Column(db.String(140), unique = True)
     content = db.Column(db.String(5000))
 
     def __init__(self, title, content):
@@ -18,14 +18,17 @@ class Article(db.Model):
     def __repr__(self):
         return "Title: {title}, content: {content}".format(title=self.title, content=self.content)
 
-@app.route('/menu')
-def menu():
-    return render_template('menu.html')
 
 @app.route('/')
 def home():
     return render_template('index.html', articles = Article.query.all())
 
+@app.route('/<int:post_id>')
+def show_post(post_id):
+    return render_template('single.html', article = Article.query.get(post_id), articles = Article.query.all())
+
+
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080,  debug=True)
+    app.run(host='0.0.0.0',port=8081,  debug=True)
